55d0723d9bee8f476ac7f2222e71676e02dc9d74
Meng-Wei Wu <mengweiwu18@gmail.com>
2023-03-07 22:26:42 -0800

    calculate fdr on sample data

--- fdr_samples.R
+++ fdr_samples.R
@@ -1,28 +1,19 @@
----
-title: "false_discovery_rate"
-author: "Meng-Wei Wu"
-date: '2023-01-09'
-output: html_document
----
-
-```{r}
 library(readr)
 library(dplyr)
 library(ggplot2)
 library(leaps)
 library(fastDummies)
 library(car)
-```
+
 
 ### Exploratory data analysis
-Explore the “autos.csv” data. Include any metrics and / or plots you find interesting.
-```{r}
 autos <- read_csv("autos.csv")
-# summary for all variables
-## numeric data
+
+### summary for all variables
+### numeric data
 summary(autos)
 
-## categorical data
+### categorical data
 # make
 ggplot(aes(x = make), data = autos) + geom_bar() + coord_flip()
 
@@ -49,26 +40,24 @@ ggplot(aes(x = engine_type), data = autos) + geom_bar()
 
 # fuel_system
 ggplot(aes(x = fuel_system), data = autos) + geom_bar()
-```
+
 
 ### Model building
-Create a linear regression model to predict price. Explain your model.
-```{r}
-# create dummy variables
+### create dummy variables
 autos_df <- dummy_cols(autos, select_columns = c("make", "fuel_type", "aspiration", "num_of_doors", "body_style", "drive_wheels", "engine_location", "engine_type", "fuel_system"), remove_selected_columns = TRUE, remove_first_dummy = TRUE)
 
-# create the model with all variables
+### create the model with all variables
 model3 <- lm(price ~ ., data = autos_df)
 summary(model3)
 
-# remove the variables which have singularity problem
+### remove the variables which have singularity problem
 autos_modified1 <- subset(autos, select = -c(fuel_system, engine_type))
 autos_modified1_df <- dummy_cols(autos_modified1, select_columns = c("make", "fuel_type", "aspiration", "num_of_doors", "body_style", "drive_wheels", "engine_location"), remove_selected_columns = TRUE, remove_first_dummy = TRUE)
 
 model3_modified1 <- lm(price ~ ., data = autos_modified1_df)
 summary(model3_modified1) 
 
-# check the multicolinearity problem by using VIF function and remove the variables with extremely high VIF
+### check the multicolinearity problem by using VIF function and remove the variables with extremely high VIF
 vif(model3_modified1)
 
 autos_modified2 <- subset(autos_modified1, select = -c(compression_ratio, fuel_type))
@@ -77,7 +66,7 @@ autos_modified2_df <- dummy_cols(autos_modified2, select_columns = c("make", "as
 model3_modified2 <- lm(price ~ ., data = autos_modified2_df)
 summary(model3_modified2) 
 
-# alpha = 0.01
+### alpha = 0.01
 pvalues_autos <- summary(model3_modified2)$coefficient[, 4]
 j <- 1
 alpha2 <- 0.1
@@ -90,11 +79,9 @@ for (j in 1:length(pvalues_autos)) {
   j = j + 1  
 }
 count2
-```
+
 
 ### Benjamini-Hochberg Procedure & False Discovery Rate
-Use the BH procedure to control the FDR with a q of 0.1. How many true discoveries do you estimate? Plot the cutoff line together with the significant and insignificant p-values.
-```{r}
 pvalues_autos_df <- data.frame(pvalues_autos)
 q <- 0.1
 n <- nrow(pvalues_autos_df)
@@ -105,7 +92,7 @@ pvalues_autos_df <- pvalues_autos_df %>%
   mutate(test = ifelse(pvalues_autos <= BHvalues, "TRUE", "FALSE"))
 head(pvalues_autos_df, 15)
 
-# count significant values
+### count significant values
 k <- 1
 count3 <- 0
 for (k in 1:nrow(pvalues_autos_df)) {
@@ -120,7 +107,7 @@ count3
 plot(pvalues_autos_df$pvalues_autos, log = "xy", col = ifelse(pvalues_autos_df$test == "TRUE", "red", "grey"), pch = 19, xlab = "rank ordered by p-values", ylab = "p-values", main = paste("FDR =", q))
 lines(1:n, q*(1:n) / n)
 
-# fdr (codes from Professor)
+### fdr
 fdr <- function(pvals, q, plotit=FALSE){
   pvals <- pvals[!is.na(pvals)]
   N <- length(pvals)
@@ -131,14 +118,13 @@ fdr <- function(pvals, q, plotit=FALSE){
   if(plotit){
     sig <- factor(pvals <= alpha)
     o <- order(pvals)
-    plot(pvals[o], log="xy", col=c("grey60","red")[sig[o]], pch=20, 
-      ylab="p-values", xlab="tests ordered by p-value", main = paste('FDR =',q))
+    plot(pvals[o], log="xy", col = c("grey60","red")[sig[o]], pch=20, 
+         ylab="p-values", xlab="tests ordered by p-value", main = paste('FDR =',q))
     lines(1:N, q*(1:N) / N)
   }
   
   return(alpha)
 }
 
-# plot the variables
+### plot the variables
 fdr(pvalues_autos, 0.1, plotit = TRUE)
-```
